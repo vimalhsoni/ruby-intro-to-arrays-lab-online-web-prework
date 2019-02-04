4a41227904b845dec48efcde76a7752358f46bb3
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-04 21:52:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -11,3 +11,16 @@ end
 def first_element(array)
   return array [0]
 end
+
+def third_element(array)
+  return array [2]
+end
+
+def last_element(array)
+  return array [-1]
+end
+
+def last_element_with_array_methods(array)
+  return array.last
+end
+  
