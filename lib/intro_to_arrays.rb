16e56474168674e97f8d44406e0796bba3cea723
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-04 21:39:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -3,7 +3,7 @@ def instantiate_new_array
   return new_array
 end
 
-def array_with_two_elements(element1, element2)
-  two_array = [element1, element2]
+def array_with_two_elements
+  two_array = ["michael","scottie"]
   return two_array
 end
